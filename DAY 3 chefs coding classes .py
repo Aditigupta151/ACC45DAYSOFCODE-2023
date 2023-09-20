c11c7b0019f2625e67dbf606fd313c4308d49109
Aditigupta151 <144876611+Aditigupta151@users.noreply.github.com>
2023-09-20 23:01:47 +0530

    Update DAY 3 chefs coding classes .py

--- DAY 3 chefs coding classes .py
+++ DAY 3 chefs coding classes .py
@@ -1,2 +1,18 @@
+# This year X no of students joined chefs coding classes
+X = input("no of students ?")
+print(X)
+
+# Y no of chairs are  already present in codeing classes
+Y = input("chairs already present ?")
+print(Y)
+
+# T is the required chairs
+T = int(X) - int(Y)
+
+if T>0 :
+   print(str("total chairs required is : ") + str(T))
+
+else :
+    print("NO chairs required")
 
 
